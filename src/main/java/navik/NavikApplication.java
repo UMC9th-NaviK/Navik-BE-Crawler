5279fc11f36f828c25c1ee416316041352d26a60
pywoo <kfdsy0103@naver.com>
2026-01-20 19:57:31 +0900

    feat: init repository

---
+++ src/main/java/navik/NavikApplication.java
@@ -0,0 +1,13 @@
+package navik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/navik/NavikApplicationTests.java
@@ -0,0 +1,13 @@
+package navik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
