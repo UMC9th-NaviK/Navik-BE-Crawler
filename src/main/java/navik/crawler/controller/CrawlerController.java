2afe59d407414994b72bd6c5b756293f37afbf73
pywoo <kfdsy0103@naver.com>
2026-01-20 20:26:10 +0900

    feat: naver ocr 상수 정의

--- src/main/java/navik/crawler/controller/CrawlerController.java
+++ src/main/java/navik/crawler/controller/CrawlerController.java
@@ -9,9 +9,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import navik.domain.crawler.service.CrawlerService;
 
-/**
- * TODO: 실 환경에서 직접 트리거 해서 메모리 확인해보고, 서버 분리 결정할 예정입니다.
- */
 @Slf4j
 @RestController
 @RequestMapping("/v1/crawler")
@@ -21,14 +18,10 @@ public class CrawlerController implements CrawlerControllerDocs {
 	private final CrawlerService crawlerService;
 
 	/**
-	 * TODO: 실 배포환경에서 직접 테스트 후 스케쥴러로 등록
+	 * TODO: 스케쥴러로 등록
 	 */
 	@PostMapping("/schedule")
 	public void triggerSchedule(@RequestParam Integer pagesToCrawl) {
 		crawlerService.scheduledCrawl(pagesToCrawl);
 	}
-
-	/**
-	 * TODO: 개발자 따로, PM, 디자이너 따로
-	 */
 }
