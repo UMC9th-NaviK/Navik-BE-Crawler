3b203903845e330aa2c5db382ad7e14d1994090a
pywoo <kfdsy0103@naver.com>
2026-03-24 18:28:22 +0900

    test: test container용 config 작성

--- src/test/java/navik/redis/config/RedisTestContainersConfig.java
+++ src/test/java/navik/redis/config/RedisTestContainersConfig.java
@@ -1,36 +1,24 @@
 package navik.redis.config;
 
-import java.time.Duration;
+import static org.testcontainers.utility.DockerImageName.*;
 
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration
-@Testcontainers
 public class RedisTestContainersConfig {
-	private static final int REDIS_PORT = 6379;
 
-	@Container
-	private static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4.1-alpine3.20")
-		.withExposedPorts(REDIS_PORT)
-		.waitingFor(Wait.forListeningPort())
-		.withStartupTimeout(Duration.ofSeconds(60));
+	private static final String REDIS_DOCKER_IMAGE = "redis:7.4.1-alpine3.20";
 
 	static {
-		redisContainer.start();
-	}
+		GenericContainer<?> REDIS_CONTAINER =
+			new GenericContainer<>(parse(REDIS_DOCKER_IMAGE))
+				.withExposedPorts(6379)
+				.withReuse(true);
 
-	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(
-			redisContainer.getHost(),
-			redisContainer.getMappedPort(REDIS_PORT)
-		);
+		REDIS_CONTAINER.start();
+
+		System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
+		System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
 	}
 }
