7a581dcf20be7ca34e0cb52ab3ff428a337c8505
pywoo <kfdsy0103@naver.com>
2026-03-25 15:11:32 +0900

    fix: processETL() 접근제어자 public 변경

--- src/main/java/navik/crawler/service/CrawlerService.java
+++ src/main/java/navik/crawler/service/CrawlerService.java
@@ -146,7 +146,7 @@ public class CrawlerService {
 	/**
 	 * 채용 공고에 대한 데이터 추출, 변환, 적재 작업을 수행하는 메서드입니다.
 	 */
-	private void processETL(WebDriverWait wait) {
+	public void processETL(WebDriverWait wait) {
 
 		// 1. 채용 공고 상세 페이지 url 유효성 검사
 		String link = crawlerDataExtractor.extractCurrentUrl(wait);
