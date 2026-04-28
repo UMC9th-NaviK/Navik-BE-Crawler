d74b2686284ce91d84392a9f45a88805b995f58f
pywoo <kfdsy0103@naver.com>
2026-04-28 18:16:55 +0900

    feat: job korea 용 bloomFilterName 정의

--- src/main/java/navik/crawler/constants/JobKoreaConstant.java
+++ src/main/java/navik/crawler/constants/JobKoreaConstant.java
@@ -4,4 +4,5 @@ public class JobKoreaConstant {
 	public static final String BASE_URL = "https://www.jobkorea.co.kr";
 	public static final String RECRUITMENT_LIST_URL = "https://www.jobkorea.co.kr/recruit/joblist?menucode=duty";
 	public static final String RECRUITMENT_DETAIL_URL_PATTERN = "https://www.jobkorea.co.kr/Recruit/GI_Read/(\\d+)";
+	public static final String BLOOM_FILTER_NAME = "bf:jobkorea:post-id";
 }
